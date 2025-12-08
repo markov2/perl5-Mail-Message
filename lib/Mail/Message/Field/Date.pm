f55f827f883bcac72b7800513335bea9b32d3122
Mark Overmeer <mark@overmeer.net>
2025-12-08 10:05:49 +0100

    ::Field::Date regex

--- lib/Mail/Message/Field/Date.pm
+++ lib/Mail/Message/Field/Date.pm
@@ -19,6 +19,7 @@ Mail::Message::Field::Date - message header field with uris
 =chapter SYNOPSIS
 
   my $f = Mail::Message::Field->new(Date => time);
+  my $date = $f->date;  # cleaned-up and validated.
 
 =chapter DESCRIPTION
 Dates are a little more tricky than it should be: the formatting permits
@@ -51,30 +52,30 @@ sub parse($)
 {	my ($self, $string) = @_;
 
 	my ($dn, $d, $mon, $y, $h, $min, $s, $z) = $string =~ m/
-		 ^ \s*
-			(?: ($dayname) \s* \, \s* )?
-			( 0?[1-9] | [12][0-9] | 3[01] ) \s*    # day
-			\s+ ( [A-Z][a-z][a-z]|[0-9][0-9] ) \s+ # month(
-				(?: 19 | 20 | ) [0-9][0-9] ) \s+     # year(
-					[0-1]?[0-9] | 2[0-3] ) \s*    # hour
-				[:.] ( [0-5][0-9] ) \s*            # minute
-			(?: [:.] ( [0-5][0-9] ) )? \s+         # second(
-				[+-][0-9]{4} | [A-Z]+ )?             # zone
+		 ^	\s*
+			(?: ($dayname) \s* \, \s* )?         # dayname (optional)
+			( 0?[1-9] | [12][0-9] | 3[01] ) \s+  # day
+			( [A-Z][a-z][a-z]|[0-9][0-9]  ) \s+  # month
+			( (?: 19 | 20 | ) [0-9][0-9]  ) \s+  # year
+			( [0-1]?[0-9] | 2[0-3] )        \s*  # hour
+				[:.] ( [0-5][0-9] )         \s*  # minute
+			(?: [:.] ( [0-5][0-9] ) )?      \s*  # second (optional)
+			( [+-][0-9]{4} | [A-Z]+ )?           # zone
 			\s*
-	 /x or return undef;
+		$ /x or return undef;
 
-	defined $dn or $dn = '';
-	$dn  =~ s/\s+//g;
-	$mon = $months[$mon-1] if $mon =~ /[0-9]+/;   # Broken mail clients
+	$dn //= '';
+	$dn   =~ s/\s+//g;
+	$mon  = $months[$mon-1] if $mon =~ /[0-9]+/;   # Broken mail clients
 
-	$y  += 2000 if $y < 50;
-	$y  += 1900 if $y < 100;
+	$y   += 2000 if $y < 50;
+	$y   += 1900 if $y < 100;
 
-	$z ||= '-0000';
-	$z   =  $tz{$z} || '-0000' if $z =~ m/[A-Z]/;
+	$z  ||= '-0000';
+	$z    = $tz{$z} || '-0000' if $z =~ m/[A-Z]/;
 
-	$self->{MMFD_date} = sprintf "%s%s%02d %s %04d %02d:%02d:%02d %s",
-		$dn, (length $dn ? ', ' : ''), $d, $mon, $y, $h, $min, $s, $z;
+	$self->{MMFD_date} = sprintf "%s%02d %s %04d %02d:%02d:%02d %s",
+		(length $dn ? "$dn, " : ''), $d, $mon, $y, $h, $min, $s // 0, $z;
 
 	$self;
 }
